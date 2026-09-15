7b1d0d68a11b31c9149de7803f11d20d68b7498d
paola-linares <paolalinaresfa@gmail.com>
2026-09-15 13:41:38 -0400

    Add game JavaScript

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
+/* =========================================
+   RWJ WASTEWISE
+   MAIN GAME JAVASCRIPT
+========================================= */
 
+console.log("RWJ WasteWise loaded!");
+
+const startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", function () {
+
+    alert("Game starting!");
+
+});
